258eaaea586b69beea99f7f1228e1faea020e060
philnat29 <philnat29@orange.fr>
2018-04-29 08:44:56 +0200

    block
    + 10 random block

--- cam.lua
+++ cam.lua
@@ -1,8 +1,8 @@
 function camload()
   camworld = {width = 4800, height = 900}
   cam = gamera.new(0, 0, 1600, 900)
-  cam:setWorld(0, 0, ground[1].w, 900)
+  cam:setWorld(0, 0, camworld.width, camworld.height)
 end
 function camupdate(dt)
-  cam:setPosition(player.x, player.y)
+  cam:setPosition(player.x + 12.5, player.y + 12.5)
 end
--- world.lua
+++ world.lua
@@ -5,18 +5,24 @@ function addworld()
   world:add(ground[1], ground[1].x, ground[1].y, ground[1].w, ground[1].h)
   grav = 200
   -- terrain
+  local num = 1
   block = {}
-  block[1] = {x = 4750, y = 800, w = 50, h = 50}
-  world:add(block[1], block[1].x, block[1].y, block[1].w, block[1].h)
+  for num = 1, 10, 1 do
+    block[num] = {x = math.random(0, 4750), y = math.random(0, 750), w = 50, h = 50}
+    world:add(block[num], block[num].x, block[num].y, block[num].w, block[num].h)
+  end
 end
 function worlddraw()
+  --perso
   love.graphics.setColor(10, 255, 30, 70)
   love.graphics.rectangle("fill", world:getRect(ground[1]))
   love.graphics.setColor(10, 255, 30)
   love.graphics.rectangle("line", world:getRect(ground[1]))
-
-  love.graphics.setColor(255, 30, 10, 70)
-  love.graphics.rectangle("fill", world:getRect(block[1]))
-  love.graphics.setColor(255, 10, 30)
-  love.graphics.rectangle("line", world:getRect(block[1]))
+  --ennemis
+  for num = 1, 10, 1 do
+    love.graphics.setColor(255, 30, 10, 70)
+    love.graphics.rectangle("fill", world:getRect(block[num]))
+    love.graphics.setColor(255, 10, 30)
+    love.graphics.rectangle("line", world:getRect(block[num]))
+  end
 end
