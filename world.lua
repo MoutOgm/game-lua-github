c43a65c4ec19a038c244f0bd8e9f5177a210513a
philnat29 <philnat29@orange.fr>
2018-04-30 09:42:44 +0200

    ennemis square
    + square = ennemis

--- world.lua
+++ world.lua
@@ -6,13 +6,20 @@ function addworld()
   grav = 200
   -- terrain
   local numblock = 1
-  local numennemis = math.random(25, 50)
   block = {}
   for numblock = 1, 10, 1 do
     block[numblock] = {x = math.random(0, 4750), y = math.random(0, 750), w = 50, h = 50}
     world:add(block[numblock], block[numblock].x, block[numblock].y, block[numblock].w, block[numblock].h)
   end
   --ennemis
+  local numennemi = 1
+  ennemis = {}
+  num = math.random(25, 50)
+  for numennemi = 1, num, 1 do
+    ennemis[numennemi] = {x = math.random(0, 4750), y = math.random(0, 750), w = 10, h = 10}
+    world:add(ennemis[numennemi], ennemis[numennemi].x, ennemis[numennemi].y, ennemis[numennemi].w, ennemis[numennemi].h)
+    num = numennemi
+  end
 
 end
 function worlddraw()
@@ -29,4 +36,10 @@ function worlddraw()
     love.graphics.rectangle("line", world:getRect(block[numblock]))
   end
   --ennemis
+  for numennemi = 1, num, 1 do
+    love.graphics.setColor(10, 200, 130, 70)
+    love.graphics.rectangle("fill", world:getRect(ennemis[numennemi]))
+    love.graphics.setColor(10, 200, 130)
+    love.graphics.rectangle("line", world:getRect(ennemis[numennemi]))
+  end
 end
