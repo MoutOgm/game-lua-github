a0ced3df9ee8c48fcfdce0a7aba25cfbc809a97a
MoutOgm <bouttier29.alex@orange.fr>
2024-11-06 21:42:28 +0100

    Removing bug debug

--- main.lua
+++ main.lua
@@ -14,86 +14,74 @@ require("save_charge/charge")
 require("cam")
 require("debug")
 function love.load()
-  width = love.graphics.getWidth()
-  height  = love.graphics.getHeight()
-  yrec = 10
-  love.window.setFullscreen(true)
+	width = love.graphics.getWidth()
+	height = love.graphics.getHeight()
+	yrec = 10
+	love.window.setFullscreen(true)
 
-  lookcharge = false
-  game = false
+	lookcharge = false
+	game = false
 
-  menuload()
-  manaload()
-  vieload()
-  levelload()
+	menuload()
+	manaload()
+	vieload()
+	levelload()
 
-  cols_len = 0
-  world = bump.newWorld()
-  grave = 10
+	cols_len = 0
+	world = bump.newWorld()
+	grave = 10
 
-  addworld()
-  playerload()
+	addworld()
+	playerload()
 
-  camload()
-
+	camload()
 end
 function love.update(dt)
-  if game == true then
-    levelupdate(dt)
-    manaupdate(dt)
-    vieupdate(dt)
-    restartupdate(dt)
-    playerupdate(dt)
+	if game == true then
+		levelupdate(dt)
+		manaupdate(dt)
+		vieupdate(dt)
+		restartupdate(dt)
+		playerupdate(dt)
 
-    camupdate(dt)
-  end
-  if menu == true then
-    menuupdate(dt)
-  end
-
-  local debuggame = json.decode(love.filesystem.read("game"))
-  local debugmana = debuggame.mana
-  local debugvie = debuggame.vie
-  local debuglevel = debuggame.level
-  local debugplayer = debuggame.player
-  print("player.x a charger = "..debugplayer.x)
-  print("player.y a charger = "..debugplayer.y)
-  print("player.x = "..player.x)
-  print("player.y = "..player.y)
-
+		camupdate(dt)
+	end
+	if menu == true then
+		menuupdate(dt)
+	end
 end
 function love.draw()
-  --montre menu
-  if menu == true then
-    menudraw()
-    restartdraw()
-  end
-  --montre jeu
-  if game == true then
-    manadraw()
-    viedraw()
-    leveldraw()
-    cam:draw(function()
-      worlddraw()
-      playerdraw()
-    end)
-  end
-  --montre game a charger
-  if lookcharge == true then
-    lookdraw()
-  end
+	--montre menu
+	if menu == true then
+		menudraw()
+		restartdraw()
+	end
+	--montre jeu
+	if game == true then
+		manadraw()
+		viedraw()
+		leveldraw()
+		cam:draw(function()
+			worlddraw()
+			playerdraw()
+		end)
+	end
+	--montre game a charger
+	if lookcharge == true then
+		lookdraw()
+	end
 end
 function love.keypressed(key)
-  --quitter
-  if key == "escape" then
-    love.event.quit()
-  end
-  --charger
-  keypressedcharge(key)
-  --voir game a charger
-  lookkey(key)
-  --entrer / sortir menu
-  keypressedmenu(key)
-  --enregistrer game
-  keypressedsave(key)
+	--quitter
+	if key == "escape" then
+		love.event.quit()
+	end
+	--charger
+	keypressedcharge(key)
+	--voir game a charger
+	lookkey(key)
+	--entrer / sortir menu
+	keypressedmenu(key)
+	--enregistrer game
+	keypressedsave(key)
 end
